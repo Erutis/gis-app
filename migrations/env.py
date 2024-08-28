959748a13e1dd4a955823680c03db82e083811b5
K Agajanian <k.agajanian@vidrovr.com>
2024-08-28 14:09:15 -0600

    url

--- migrations/env.py
+++ migrations/env.py
@@ -7,6 +7,8 @@ from alembic import context
 
 from app.tables import FeedItem, Trajectory, metadata
 
+import os
+
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
 config = context.config
@@ -40,7 +42,7 @@ def run_migrations_offline() -> None:
     script output.
 
     """
-    url = config.get_main_option("sqlalchemy.url")
+    url = os.getenv("DATABASE_URL")
     context.configure(
         url=url,
         target_metadata=target_metadata,
