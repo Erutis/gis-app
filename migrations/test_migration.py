bd5499a531ba3cd149f2c1bb9a516705981509a3
K Agajanian <41064815+Erutis@users.noreply.github.com>
2024-09-05 15:33:03 -0400

    Side chore (#5)
    A mess, but progress was made.

--- migrations/test_migration.py
+++ migrations/test_migration.py
@@ -5,47 +5,58 @@
 # Created: 4/18/24
 
 # Standard libraries
+import os
 import random
+import sys
 import uuid
 
-# from datetime import datetime
-
 # External libraries
+from sqlalchemy import create_engine, select
 from sqlalchemy.orm import sessionmaker
 
+# Add the parent directory to the Python path
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
+
 # Internal libraries
-from db_setup import Trajectory, engine_go_vroom
+from app.tables import Trajectory, FeedItem
 
 
 # Pre-determined areas for random data generation
 central_park = {"lon": (-73.973, -73.958), "lat": (40.765, 40.800)}
 northeast = {"lon": (-70, -75), "lat": (40, 43)}
 
+url = os.getenv("DATABASE_URL")
+engine = create_engine(url)
 
-def main():
+Session = sessionmaker(bind=engine)
+session = Session()
+
+
+def add_db_rows():
     """Enter sample data into Trajectory table."""
-    engine = engine_go_vroom()
-    Session = sessionmaker(bind=engine)
-    session = Session()
 
-    for _ in range(5):
-        try:
+    sample_data = []
+
+    try:
+        for _ in range(5):
             # Create rows from sample data
             linestring, geom_type = create_sample_linestring(central_park)
 
-            sample_data = [
-                Trajectory(
-                    geom=f"SRID=4326;{geom_type}({linestring})",
-                    feed_item_id=uuid.uuid4(),
-                )
-            ]
+            feed_item = FeedItem(id=uuid.uuid4(), name="hurhur")
 
-            session.add_all(sample_data)
-            session.commit()
+            trajectory = Trajectory(
+                geom=f"SRID=4326;{geom_type}({linestring})",
+                feed_item_id=feed_item.id,
+            )
 
-        except Exception as e:
-            print(e)
-            continue
+            sample_data.append(trajectory)
+            sample_data.append(feed_item)
+
+        session.add_all(sample_data)
+        session.commit()
+
+    except Exception as e:
+        print(e)
 
 
 def create_sample_linestring(area):
@@ -73,5 +84,16 @@ def create_sample_linestring(area):
     return linestring, geom_type
 
 
+def retrieve_row():
+    """Retrieve recently created row."""
+    q = select(FeedItem)
+    with session as s:
+        feed_items = s.execute(q).scalars().all()
+
+    print(f"Retrieved feed item: {feed_items}")
+
+
 if __name__ == "__main__":
-    main()
+    add_db_rows()
+    retrieve_row()
+    print("DB commit successful.")
