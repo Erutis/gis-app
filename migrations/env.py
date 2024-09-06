c542eab76c7ed7c6d6570d6d60f452b8a8fd400a
K Agajanian <k.agajanian@vidrovr.com>
2024-09-06 14:31:12 -0400

    what if this makes it fail

--- migrations/env.py
+++ migrations/env.py
@@ -17,7 +17,7 @@ config = context.config
 if config.config_file_name is not None:
     fileConfig(config.config_file_name)
 
-
+# what if
 # add your model's MetaData object here
 # for 'autogenerate' support
 # from myapp import mymodel
