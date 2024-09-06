3a9123700851a3f0a9c797f820e6b2a2a9ed6099
K Agajanian <k.agajanian@vidrovr.com>
2024-09-06 14:57:50 -0400

    hey yo

--- app/tables.py
+++ app/tables.py
@@ -68,3 +68,4 @@ class Trajectory(GISBase):
     __table_args__ = {"schema": "gps"}
     geom = mapped_column(Geometry("GEOMETRYZM", spatial_index=False))
     feed_item_id = mapped_column(ForeignKey(FeedItem.id))
+    new_column = mapped_column(String)
