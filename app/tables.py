35f2869a9ad71602a29f18ae85a0fccb659a58e2
K Agajanian <k.agajanian@vidrovr.com>
2024-09-06 13:02:01 -0400

    what do you want

--- app/tables.py
+++ app/tables.py
@@ -68,4 +68,4 @@ class Trajectory(GISBase):
     __table_args__ = {"schema": "gps"}
     geom = mapped_column(Geometry("GEOMETRYZM", spatial_index=False))
     feed_item_id = mapped_column(ForeignKey(FeedItem.id))
-    some_new_field = mapped_column(String)
+    some_new_field_ERRRRR = mapped_column(String)
